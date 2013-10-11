4687b84eebaed26ee1d51d8f8c3e25f18c64dd08
tntC4stl3 <blackhumour.gj@gmail.com>
2013-10-11 20:43:22 +0800

    第一阶段完成
    spider.py -u http://www.sina.com.cn -d 2 可以在单线程的情况下，抓取新浪两级深度

--- spider.py
+++ spider.py
@@ -5,6 +5,9 @@
 import argparse
 import sqlite3
 import logging
+import requests
+from bs4 import BeautifulSoup
+from Queue import Queue
 
 class Crawler(object):
 	"""Main part, carwl the site"""
@@ -23,32 +26,96 @@ class Crawler(object):
 		self.keyword = args['keyword']
 		# 是否自测
 		self.testself = args['testself']
-		# 待访问的链接，用集合来去重
+		# 当前层待访问的链接，用集合来去重
 		self.unvisitedUrl = set()
+		self.unvisitedUrl.add(args['url'])
 		# 已访问的链接
 		self.visitedUrl = set()
+		# http header
+		self.header = {
+			'Accetpt': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accetpt-Encoding': 'gzip,deflate,sdch',
+			'Connection': 'keep-alive',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
+		}
+		self.q = Queue()
+		# 连接数据库
+		self.connDB()
 
 	def start(self):
-		pass
+		while self.current_deepth <= self.max_deepth:
+			print "current deepeh: %d" % self.current_deepth
+			self._taskQueue()
+			while not self.q.empty():
+				url = self.q.get()
+				self._getLinks(url)
+			self.current_deepth += 1
+		self.closeDB()
 
-	# 连接数据库
+	def _fetchPage(self, url, retry=3):
+		'''获取页面内容'''
+		try:
+			r = requests.get(url, headers=self.header, timeout=10)
+			if r.status_code == requests.codes.ok:
+				source = r.text
+				self.writeDB(url, source)
+				return source
+			else:
+				pass
+				# log.warning('[WARNING][Open failed][status code:%d URL:%s]' % (r.status_code, self.url)
+		except Exception, e:
+			print e
+			if retry>0:
+				return self._fetchPage(url, retry-1)
+
+	def _getLinks(self, url):
+		'''从页面源代码获取所有链接'''
+		source = self._fetchPage(url=url)
+		if not source:
+			self.visitedUrl.add(url)
+			return
+		try:
+			soup = BeautifulSoup(source)
+		except Exception, e:
+			print e
+				#log.warning('[WARNING][Get href faild][URL:%s]' % self.url)
+			return
+		a_tags = soup.find_all('a', href=True)
+		for a_tag in a_tags:
+			link = a_tag.get('href')
+			if link in self.visitedUrl or link in self.unvisitedUrl:
+				pass
+			else:
+				self.unvisitedUrl.add(link)
+
+	def _taskQueue(self):
+		'''添加任务队列'''
+		while self.unvisitedUrl:
+			url = self.unvisitedUrl.pop()
+			self.q.put(url)
+			self.visitedUrl.add(url)
+
 	def connDB(self):
+		'''连接数据库'''
 		try:
 			self.conn = sqlite3.connect(self.dbfile)
-			self.conn.execute("DROP TABLE IF EXIST pages;")
+			self.conn.execute("DROP TABLE IF EXISTS pages;")
 			self.conn.execute('''CREATE TABLE pages
 								(id INTEGER PRIMARY KEY AUTOINCREMENT,
 								 url TEXT,
-								 source TEXT)''')
+								 source TEXT);''')
 			self.conn.commit()
+			print 'Connect db success.'
 		except Exception, e:
+			logging.info('Conncet db Failed.')
 			self.conn = None
 
-	# 将抓取到的数据写入数据库
 	def writeDB(self, url, source):
+		'''将抓取到的数据写入数据库'''
 		try:
 			self.conn.execute("INSERT INTO pages(url, source) VALUES (?, ?)", (url, source))
 			self.conn.commit()
+			print "insert success."
 		except Exception, e:
 			print "Commit error: %s" % e
 
@@ -63,14 +130,16 @@ def get_parser():
 	parser = argparse.ArgumentParser(description='A simple web crawler')
 	parser.add_argument('-u', metavar='URL', dest='url', type=str, 
 						help='specify the url start to crawl')
-	parser.add_argument('-d', metavar='DEEPTH', dest='deepth', default=3, 
+	parser.add_argument('-d', metavar='DEEPTH', dest='deepth', default=3, type=int,
 						help='specify the max deep')
-	parser.add_argument('--thread', default=10, type=int, 
+	parser.add_argument('--threads', default=10, type=int, 
 						help='specify the number of threads (default: 10)')
-	parser.add_argument('--dbfile', default='data.db', 
+	parser.add_argument('--dbfile', default='data.db', type=str
 						help='sqlite file to store data')
 	parser.add_argument('--key', metavar='KEYWORD', dest='keyword', type=str, 
 						help='keyword in page (optional)')
+	parser.add_argument('--logfile', default='logging.log', dest='logfile', type=str, 
+						help='the path to store log file')
 	parser.add_argument('-l', metavar='LOG_LEVEL', dest='level', type=int, 
 						help='the detail level of logfile, the larger, the more detail (optional)')
 	parser.add_argument('--testself', action='store_true', 
@@ -88,6 +157,8 @@ def command_line_runner():
 		return 
 	if not args['url'].startswith('http'):
 		args['url'] = 'http://' + args['url']
+	crawler = Crawler(args)
+	crawler.start()
 		
 if __name__ == '__main__':
 	command_line_runner()
